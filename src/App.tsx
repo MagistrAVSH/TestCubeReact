c4e1cd905c978c361e039566377665f677504efa
MagistrAVSH <art046@hotmail.com>
2024-08-05 15:15:01 +0400

    cube unity webgl

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Unity, useUnityContext} from "react-unity-webgl";
 
 function App() {
+
+  const { unityProvider, isLoaded, loadingProgression, initialisationError } = useUnityContext({
+    loaderUrl: "Build/Build.loader.js",
+    dataUrl: "Build/Build.data.unityweb",
+    frameworkUrl: "Build/Build.framework.js.unityweb",
+    codeUrl: "Build/Build.wasm.unityweb",
+  });
+
+  // We'll round the loading progression to a whole number to represent the
+  // percentage of the Unity Application that has loaded.
+  const loadingPercentage = Math.round(loadingProgression * 100);
+
+  console.log(initialisationError)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!isLoaded && (
+            // We'll conditionally render the loading overlay if the Unity
+            // Application is not loaded.
+            <div className="loading-overlay">
+              <p>Loading... ({loadingPercentage}%)</p>
+            </div>
+        )}
+        <Unity className="unity" unityProvider={unityProvider} />
       </header>
     </div>
   );
